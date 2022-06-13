77886c8ce6b2c39761fab319f1d132ae43986fc2
BrenoAlexandre <bdebreno19@gmail.com>
2022-06-13 18:39:29 -0300

    CRUD 'C & U' ain't working
    Change-Id: If8e36a7ae09a1df586a50a2ee19b1368370d5e55

--- src/pages/Users/Actions/index.tsx
+++ src/pages/Users/Actions/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import * as yup from 'yup';
-import { Form, Formik } from 'formik';
+import { useFormik } from 'formik';
 import { Row, Col } from 'react-bootstrap';
 import { useHistory, useParams } from 'react-router-dom';
 import classNames from 'classnames';
+import InputMask from 'react-input-mask';
 import Section from '../../../components/Section';
 import Text from '../../../components/Text';
 import Button from '../../../components/Button';
@@ -24,6 +25,14 @@ const createSchema = yup.object().shape({
     .min(6, 'Min. 6 caracteres')
     .required('Campo obrigatório')
     .oneOf([yup.ref('password'), null], 'Passwords must match'),
+});
+
+const updateSchema = yup.object().shape({
+  name: yup.string().min(2, 'Min. 2 caracteres').max(50, 'Máximo 50 caracteres').required('Campo obrigatório'),
+  cpf: yup.string().length(11, 'CPF deve conter 11 dígitos').required('Campo obrigatório'),
+  birthdate: yup.date().required('Campo obrigatório'),
+  role: yup.mixed<keyof typeof ERole>().oneOf(Object.values([ERole.admin, ERole.user])),
+  obs: yup.string().max(500, 'Max. 500 caracteres'),
 });
 
 interface ICreate {
@@ -52,7 +61,7 @@ const Create: React.FunctionComponent = (): React.ReactElement => {
   const [loader, setLoader] = useState<boolean>(false);
   const [initialValues, setInitialValues] = useState(defaultValue as ICreate);
 
-  const submitHandler = async (values: ICreate): Promise<void> => {
+  async function submitHandler(values: ICreate): Promise<void> {
     try {
       setLoader(true);
       const { name, cpf, birthdate, role, obs, password, confirmPassword } = values;
@@ -71,7 +80,7 @@ const Create: React.FunctionComponent = (): React.ReactElement => {
       setLoader(false);
       toastMsg(ToastType.Error, (error as Error).message);
     }
-  };
+  }
 
   useEffect(() => {
     let isCleaningUp = false;
@@ -104,6 +113,13 @@ const Create: React.FunctionComponent = (): React.ReactElement => {
     };
   }, [history, id]);
 
+  const formik = useFormik({
+    initialValues,
+    validationSchema: id ? createSchema : updateSchema,
+    enableReinitialize: true,
+    onSubmit: submitHandler,
+  });
+
   return (
     <Section
       className="create"
@@ -119,144 +135,146 @@ const Create: React.FunctionComponent = (): React.ReactElement => {
       </Row>
       <Row>
         <Col md={8}>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={createSchema}
-            enableReinitialize
-            onSubmit={submitHandler}
-          >
-            {({ values, handleChange, handleSubmit }) => (
-              <Form onSubmit={handleSubmit}>
-                <Row>
-                  <Col md={12} className="mb-3">
-                    <label htmlFor="name" className="w-100">
-                      Nome
-                      <br />
-                      <input
-                        date-testid="test-inputName"
-                        id="name"
-                        name="name"
-                        disabled={!!id}
-                        value={`${values.name}`}
-                        onChange={handleChange}
-                        placeholder="Nome do funcionário"
-                        className={classNames('form-control')}
-                      />
-                    </label>
-                  </Col>
+          <form onSubmit={formik.handleSubmit}>
+            <Row>
+              <Col md={12} className="mb-3">
+                <label htmlFor="name" className="w-100">
+                  Nome
+                  <br />
+                  <input
+                    date-testid="test-inputName"
+                    id="name"
+                    name="name"
+                    disabled={!!id}
+                    value={`${formik.values.name}`}
+                    onChange={formik.handleChange}
+                    placeholder="Nome do funcionário"
+                    className={classNames('form-control')}
+                  />
+                </label>
+              </Col>
 
-                  <Col md={4} className="mb-3">
-                    <label htmlFor="cpf" className="w-100">
-                      CPF
-                      <br />
+              <Col md={4} className="mb-3">
+                <label htmlFor="cpf" className="w-100">
+                  CPF
+                  <br />
+                  <InputMask
+                    mask="999.999.999-99"
+                    maskPlaceholder=""
+                    value={`${formik.values.cpf}`}
+                    onChange={formik.handleChange}
+                    disabled={!!id}
+                  >
+                    {() => (
                       <input
                         data-testid="test-inputCpf"
                         id="cpf"
                         name="cpf"
-                        disabled={!!id}
-                        value={`${values.cpf}`}
-                        onChange={handleChange}
                         placeholder="CPF do funcionário"
                         className={classNames('form-control')}
                       />
-                    </label>
-                  </Col>
+                    )}
+                  </InputMask>
+                </label>
+              </Col>
 
-                  <Col md={4} className="mb-3">
-                    <div className="w-100">
-                      Data de Nascimento
+              <Col md={4} className="mb-3">
+                <div className="w-100">
+                  Data de Nascimento
+                  <br />
+                  <input
+                    data-testid="test-inputBirthdate"
+                    id="birthdate"
+                    name="birthdate"
+                    type="date"
+                    disabled={!!id}
+                    value={`${formik.values.birthdate}`}
+                    onChange={formik.handleChange}
+                    className={classNames('form-control')}
+                  />
+                </div>
+              </Col>
+
+              <Col md={4} className="mb-3">
+                <label htmlFor="role" className="w-100">
+                  Papel do funcionário
+                  <br />
+                  <select
+                    data-testid="test-selectRole"
+                    id="role"
+                    name="role"
+                    value={`${formik.values.role}`}
+                    onChange={formik.handleChange}
+                    className={classNames('form-control')}
+                  >
+                    <option>-- Selecione --</option>
+                    <option value={ERole.admin}>Administrador</option>
+                    <option value={ERole.user}>Colaborador</option>
+                  </select>
+                </label>
+              </Col>
+
+              {!id && (
+                <>
+                  <Col md={6} className="mb-3">
+                    <label htmlFor="password" className="w-100">
+                      Senha
                       <br />
                       <input
-                        data-testid="test-inputBirthdate"
-                        id="birthdate"
-                        name="birthdate"
-                        type="date"
-                        disabled={!!id}
-                        value={`${values.birthdate}`}
-                        onChange={handleChange}
-                        className={classNames('form-control')}
-                      />
-                    </div>
-                  </Col>
-
-                  <Col md={4} className="mb-3">
-                    <label htmlFor="role" className="w-100">
-                      Papel do funcionário
-                      <br />
-                      <select
-                        data-testid="test-selectRole"
-                        id="role"
-                        name="role"
-                        value={`${values.role}`}
-                        onChange={handleChange}
-                        className={classNames('form-control')}
-                      >
-                        <option>-- Selecione --</option>
-                        <option value={ERole.admin}>Administrador</option>
-                        <option value={ERole.user}>Colaborador</option>
-                      </select>
-                    </label>
-                  </Col>
-
-                  {!id && (
-                    <>
-                      <Col md={6} className="mb-3">
-                        <label htmlFor="password" className="w-100">
-                          Senha
-                          <br />
-                          <input
-                            data-testid="test-inputPassword"
-                            id="password"
-                            name="password"
-                            type="password"
-                            placeholder="Senha"
-                            className={classNames('form-control')}
-                          />
-                        </label>
-                      </Col>
-
-                      <Col md={6} className="mb-3">
-                        <label htmlFor="confirmPassword" className="w-100">
-                          Confirmar senha
-                          <br />
-                          <input
-                            data-testid="test-inputConfirmPassword"
-                            id="confirmPassword"
-                            name="confirmPassword"
-                            type="password"
-                            placeholder="Confirmar senha"
-                            className={classNames('form-control')}
-                          />
-                        </label>
-                      </Col>
-                    </>
-                  )}
-
-                  <Col md={12} className="mb-3">
-                    <label htmlFor="obs" className="w-100">
-                      Observações
-                      <br />
-                      <textarea
-                        data-testid="test-inputObs"
-                        id="obs"
-                        name="obs"
-                        value={`${values.obs}`}
-                        onChange={handleChange}
-                        placeholder="Observações sobre o funcionário..."
+                        data-testid="test-inputPassword"
+                        id="password"
+                        name="password"
+                        type="password"
+                        value={`${formik.values.password}`}
+                        onChange={formik.handleChange}
+                        placeholder="Senha"
                         className={classNames('form-control')}
                       />
                     </label>
                   </Col>
 
-                  <Col md={12} className="mb-3">
-                    <Button type="submit" cy="test-login" variant="primary" disabled={!!loader}>
-                      {id ? 'Editar' : 'Criar'} funcionário
-                    </Button>
+                  <Col md={6} className="mb-3">
+                    <label htmlFor="confirmPassword" className="w-100">
+                      Confirmar senha
+                      <br />
+                      <input
+                        data-testid="test-inputConfirmPassword"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        type="password"
+                        value={`${formik.values.confirmPassword}`}
+                        onChange={formik.handleChange}
+                        placeholder="Confirmar senha"
+                        className={classNames('form-control')}
+                      />
+                    </label>
                   </Col>
-                </Row>
-              </Form>
-            )}
-          </Formik>
+                </>
+              )}
+
+              <Col md={12} className="mb-3">
+                <label htmlFor="obs" className="w-100">
+                  Observações
+                  <br />
+                  <textarea
+                    data-testid="test-inputObs"
+                    id="obs"
+                    name="obs"
+                    value={`${formik.values.obs}`}
+                    onChange={formik.handleChange}
+                    placeholder="Observações sobre o funcionário..."
+                    className={classNames('form-control')}
+                  />
+                </label>
+              </Col>
+
+              <Col md={12} className="mb-3">
+                <Button type="submit" cy="test-login" variant="primary" disabled={!!loader}>
+                  {id ? 'Editar' : 'Criar'} funcionário
+                </Button>
+              </Col>
+            </Row>
+          </form>
         </Col>
       </Row>
     </Section>
